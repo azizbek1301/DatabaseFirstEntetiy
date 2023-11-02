5faa89691c1dd960dcc586973d5de51825889930
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-02 18:00:47 +0500

    Use Scaffold-DbContext

---
+++ DatabaseFirstEntetiy/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstEntetiy.Models;
+
+public partial class Student
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public int? Year { get; set; }
+}
---
+++ DatabaseFirstEntetiy/Models/UniversityContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirstEntetiy.Models;
+
+public partial class UniversityContext : DbContext
+{
+    public UniversityContext()
+    {
+    }
+
+    public UniversityContext(DbContextOptions<UniversityContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ArchiveStudent> ArchiveStudents { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;database=University;trusted_connection=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ArchiveStudent>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable(tb => tb.HasTrigger("trInsertStudent"));
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
